24336a9af85c77a4f1b66f82bf2e93a9f320eaec
Malak <malakbadawyy@gmail.com>
2022-01-18 12:19:35 +0000

    Display recipes corresponding to the category

--- CookBook/Scenes/Recipes/RecipesViewController.swift
+++ CookBook/Scenes/Recipes/RecipesViewController.swift
@@ -8,22 +8,78 @@
 import UIKit
 
 class RecipesViewController: UIViewController {
-    var viewModel: RecipesViewModel?
-    var category: String = ""
+    // MARK: - IBOutlets
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Public Properties
+    
+    var category: String = ""
+    
+    // MARK: - Private Properties
+    
+    private var viewModel: RecipesViewModel?
+    private var recipes: [Recipe]?
+
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCategoryTitle()
+        configureTableView()
         configureViewModel()
         loadRecipes()
     }
+    
+    // MARK: - Private Functions
     
     private func configureViewModel() {
         viewModel = RecipesViewModel()
     }
     
+    private func setCategoryTitle() {
+        self.title = category
+    }
+
     private func loadRecipes() {
-        viewModel?.loadRecipes(for: category) { recipes, error in
-            print(recipes)
+        viewModel?.loadRecipes(for: category) { [weak self] recipes, error in
+            self?.recipes = recipes
+            self?.reloadTableView()
         }
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "ImageTitleTableViewCell", bundle: nil), forCellReuseIdentifier: "ImageTitleTableViewCell")
+    }
+    
+    private func reloadTableView() {
+        DispatchQueue.main.async { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+}
+
+extension RecipesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ImageTitleTableViewCell") as? ImageTitleTableViewCell
+        let recipe = recipes?[indexPath.row]
+        cell?.selectionStyle = .none
+        cell?.configure(
+            imageURL: URL(string: recipe?.thumbnail ?? ""),
+            title: recipe?.title ?? ""
+        )
+        return cell ?? UITableViewCell()
+    }
+}
+
+extension RecipesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
 }
