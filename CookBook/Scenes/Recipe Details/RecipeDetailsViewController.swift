b09fbdb30ded83af7974d9995031644907e8a231
Malak <malakbadawyy@gmail.com>
2022-01-19 22:56:40 +0000

    Add API call to load recipe details

--- CookBook/Scenes/Recipe Details/RecipeDetailsViewController.swift
+++ CookBook/Scenes/Recipe Details/RecipeDetailsViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class RecipeDetailsViewController: UIViewController {
+    // MARK: - Public Properties
+    
     var recipeID: String = ""
+    
+    // MARK: - Private Properties
+    
+    private var viewModel: RecipeDetailsViewModel?
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        RecipeDetailsViewModel().loadRecipeDetails(for: recipeID)
+        configureViewModel()
+        loadRecipeDetails()
+    }
+    
+    // MARK: - Private Properties
+    
+    private func configureViewModel() {
+        viewModel = RecipeDetailsViewModel()
+    }
+    
+    private func loadRecipeDetails() {
+        viewModel?.loadRecipeDetails(for: recipeID) { recipeDetails, ingredients, error in
+            print(recipeDetails)
+        }
     }
 }
